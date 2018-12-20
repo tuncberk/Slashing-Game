61f17654bf3fd280858bc50b4b12fed5fa715ca9
tuncberk <berktunc@sabanciuniv.edu>
2018-12-20 16:09:55 +0300

    update

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+
+    public InputField nameField;
+    public InputField passwordField;
+
+    public Button submitButton;
+
+    public void CallLogin()
+    {
+        StartCoroutine(LoginTherapist());
+    }
+
+    IEnumerator LoginTherapist()
+    {
+        string targetURL = "http://localhost:8080/RunForestRun/login.php";
+        Debug.Log("Logining " + targetURL);
+
+
+        WWWForm form = new WWWForm();
+        form.AddField("name", nameField.text);
+        form.AddField("password", passwordField.text);
+
+        WWW www = new WWW(targetURL, form);
+        yield return www;
+
+        Debug.Log(www.text);
+        if (www.text != "0")
+        {
+           // DBManager.username = nameField.text;
+            //DBManager.score = int.Parse(www.text.Split('\t')[1]); //pass score in second chuck
+            UnityEngine.SceneManagement.SceneManager.LoadScene(4);
+        }
+        else
+        {
+            Debug.Log("User login failed. Error #" + www.text);
+        }
+
+
+    }
+
+    public void VerifyInputs()
+    {
+        submitButton.interactable = (nameField.text.Length >= 3 && passwordField.text.Length >= 3);
+    }
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameObject faderObj;
+    private Color fadeTransparency = new Color(0, 0, 0, .04f);
+    public Image faderImg;
+    public bool gameOver = false;
+    private string currentScene;
+    private AsyncOperation async;
+    public float fadeSpeed = .02f;
+
+
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = GetComponent<GameManager>();
+            SceneManager.sceneLoaded += OnLevelFinishedLoading;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ReturnToMenu();
+        }
+    }
+    public void LoadScene(string sceneName) {
+		instance.StartCoroutine(Load(sceneName));
+		instance.StartCoroutine(FadeOut(instance.faderObj, instance.faderImg));
+	}
+    IEnumerator Load(string sceneName) {
+		async = SceneManager.LoadSceneAsync(sceneName);
+		async.allowSceneActivation = false;
+		yield return async;
+		isReturning = false;
+    }
+    // Get the current scene name
+    public string CurrentSceneName
+    {
+        get
+        {
+            return currentScene;
+        }
+    }
+    private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        currentScene = scene.name;
+        instance.StartCoroutine(FadeIn(instance.faderObj, instance.faderImg));
+    }
+    public void ExitGame()
+    {
+        // If we are running in a standalone build of the game
+#if UNITY_STANDALONE
+        // Quit the application
+        Application.Quit();
+#endif
+
+        // If we are running in the editor
+#if UNITY_EDITOR
+        // Stop playing the scene
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+    private bool isReturning = false;
+    public void ReturnToMenu()
+    {
+        if (isReturning)
+        {
+            return;
+        }
+
+        if (CurrentSceneName != "MainMenu")
+        {
+            StopAllCoroutines();
+            LoadScene("MainMenu");
+            isReturning = true;
+        }
+    }
+    IEnumerator FadeOut(GameObject faderObject, Image fader)
+    {
+        faderObject.SetActive(true);
+        while (fader.color.a < 1)
+        {
+            fader.color += fadeTransparency;
+            yield return new WaitForSeconds(fadeSpeed);
+        }
+        ActivateScene(); //Activate the scene when the fade ends
+    }
+    // Allows the scene to change once it is loaded
+    public void ActivateScene()
+    {
+        async.allowSceneActivation = true;
+    }
+    // Iterate the fader transparency to 0%
+    IEnumerator FadeIn(GameObject faderObject, Image fader)
+    {
+        while (fader.color.a > 0)
+        {
+            fader.color -= fadeTransparency;
+            yield return new WaitForSeconds(fadeSpeed);
+        }
+        faderObject.SetActive(false);
+    }
+}
